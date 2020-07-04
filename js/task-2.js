fb899334e6453c0a85b07c8b626240186e4748ce
roman8301 <roman8301@gmail.com>
2020-07-04 14:37:07 +0300

    Task 2 and 3 completed

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,8 @@
 'use strict';
+const total = 100;
+const ordered = 50;
+if (ordered > total) {
+	console.log ("Not enough goods in stock!");
+} else {
+	console.log ("The order is placed, the manager will contact you");
+}
--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,12 @@
 'use strict';
+const ADMIN_PASSWORD = 'jqueryismyjam';
+let message;
+message = prompt('Enter your password:');
+if (message === null) {
+	message = 'Canceled by user!';
+} else if (message === ADMIN_PASSWORD) {
+	message = 'Welcome!';
+} else {
+	message = 'Access denied, wrong password!';
+}
+alert (message);
