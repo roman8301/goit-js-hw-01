d9332cff481985669fe2f4a29bdac8392cbeafa3
roman8301 <roman8301@gmail.com>
2020-07-04 13:56:14 +0300

    Task one completed

---
+++ js/task-1.js
@@ -0,0 +1,6 @@
+'use strict';
+const name = 'Shield generator';
+let prise = 1000;
+console.log (`Selected ${name}, unit price ${prise} credits`);
+prise *=2;
+console.log (`Selected ${name}, unit price ${prise} credits`);
