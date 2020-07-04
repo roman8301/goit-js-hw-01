09afdf12f97b2ccf46a1ed3712ef25f590e343b0
roman8301 <roman8301@gmail.com>
2020-07-04 20:06:11 +0300

    Task 5 completed

--- js/task-5.js
+++ js/task-5.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const china = 100;
+const chile= 250;
+const australia = 170;
+const india = 80;
+const jamaica = 120;
+let country = prompt('Введите страну для доставки:');
+
+if (country === null) {
+	console.log('Отменено пользователем!');
+} else {
+	switch (country.toLocaleLowerCase()) {
+		case 'китай':
+			console.log(`Доставка в страну ${country} будет стоить ${china} кредитов`);
+			break;
+		case 'чили':
+			console.log(`Доставка в страну ${country} будет стоить ${chile} кредитов`);
+			break;
+		case 'австралия':
+			console.log(`Доставка в страну ${country} будет стоить ${australia} кредитов`);
+			break;
+		case 'индия':
+			console.log(`Доставка в страну ${country} будет стоить ${india} кредитов`);
+			break;
+		case 'ямайка':
+			console.log(`Доставка в страну ${country} будет стоить ${jamaica} кредитов`);
+			break;
+		default:
+			console.log(`В вашей стране доставка не доступна`);
+	}
+}
